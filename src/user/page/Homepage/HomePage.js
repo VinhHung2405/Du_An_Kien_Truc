757e9c12573ee108c566d3003114d3677f871063
VinhHung2405 <hungle14431@gmail.com>
2024-02-27 12:18:48 +0700

    add component

--- src/user/page/Homepage/HomePage.js
+++ src/user/page/Homepage/HomePage.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Banner from './Banner/Banner'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='container'> 
+      <Banner/>
+    </div>
   )
 }
